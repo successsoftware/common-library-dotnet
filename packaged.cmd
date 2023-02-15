3214c1505956cd59cb731699c8141bf2e1126883
Success Software Services <116863270+successsoftware@users.noreply.github.com>
2023-02-15 08:51:06 +0700

    Delete packaged.cmd

--- packaged.cmd
+++
@@ -1,9 +0,0 @@
-dotnet nuget push "D:\Projects\Success Projects\common-library-dot-net\SSS.Common\bin\Release\SSS.CommonLib.1.2.1.nupkg" --api-key oy2a65arnyyq7ybsvtaadpangdz7rwnudflpawz5fpcioi --source https://api.nuget.org/v3/index.json
-
-dotnet nuget push "D:\Projects\Success Projects\common-library-dot-net\SSS.AspNetCore.Extensions\bin\Release\SSS.AspNetCore.Extensions.1.2.2.nupkg" --api-key oy2a65arnyyq7ybsvtaadpangdz7rwnudflpawz5fpcioi --source https://api.nuget.org/v3/index.json
-
-dotnet nuget push "D:\Projects\Success Projects\common-library-dot-net\SSS.AspNetCore.Extensions.Cookies\bin\Release\SSS.AspNetCore.Extensions.Cookies.1.2.0.nupkg" --api-key oy2a65arnyyq7ybsvtaadpangdz7rwnudflpawz5fpcioi --source https://api.nuget.org/v3/index.json
-
-dotnet nuget push "D:\Projects\Success Projects\common-library-dot-net\SSS.AspNetCore.Extensions.Jwt\bin\Release\SSS.AspNetCore.Extensions.Jwt.1.2.0.nupkg" --api-key oy2a65arnyyq7ybsvtaadpangdz7rwnudflpawz5fpcioi --source https://api.nuget.org/v3/index.json
-
-dotnet nuget push "D:\Projects\Success Projects\common-library-dot-net\SSS.EntityFrameworkCore.Extensions\bin\Release\SSS.EntityFrameworkCore.Extensions.1.2.5.nupkg" --api-key oy2a65arnyyq7ybsvtaadpangdz7rwnudflpawz5fpcioi --source https://api.nuget.org/v3/index.json
